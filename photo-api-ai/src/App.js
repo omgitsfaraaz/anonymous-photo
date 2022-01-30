6a794aea774bc74f77126b8b8114c8cf73ada381
mohsin16 <mohsin@theretailinsights.com>
2022-01-30 14:03:56 +0530

    first commit

--- photo-api-ai/src/App.js
+++ photo-api-ai/src/App.js
@@ -1,9 +1,32 @@
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [image, setImage] = useState('')
+
+  const handleChange = () => {
+    axios
+      .get(
+        'https://api.generated.photos/api/v1/faces?api_key=toGsTrSOE541OMVkIsLgMQ&order_by=random'
+      )
+      .then(res => {
+        const uri = res.data.faces[0].urls[4][512]
+        uri && setImage(uri)
+      })
+      .catch(err => {
+        console.log(err.message);
+      })
+  };
+
+
   return (
     <div className="App">
-      <h1>What us uppp??</h1>
+      <h1>Random AI Photo generator</h1>
+      {image && <img src={image} alt='AI face' />}
+      <button type='button' onClick={handleChange}>
+        New Image
+      </button>
     </div>
   );
 }
